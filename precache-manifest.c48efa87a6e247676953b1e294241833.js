f20a55c7f467d9f5c1e3a1205cd3742425cac9d0
suresh <suresh_kumar@external.mckinsey.com>
2019-11-20 20:14:15 +0530

    Updates

---
+++ precache-manifest.c48efa87a6e247676953b1e294241833.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "a346d377438da5d1d71c9a453ab7a9a2",
+    "url": "/blue-stack/static/media/logo.a346d377.svg"
+  },
+  {
+    "revision": "e028057c57efe7e87def",
+    "url": "/blue-stack/static/css/main.f5f228fa.chunk.css"
+  },
+  {
+    "revision": "172833553b6522a67bc2",
+    "url": "/blue-stack/static/js/runtime~main.80ca984f.js"
+  },
+  {
+    "revision": "e51f42c349763884e809677c9634b288",
+    "url": "/blue-stack/static/media/img5.e51f42c3.png"
+  },
+  {
+    "revision": "2d7f602aa6dedd9788de",
+    "url": "/blue-stack/static/js/2.3fea2a13.chunk.js"
+  },
+  {
+    "revision": "e028057c57efe7e87def",
+    "url": "/blue-stack/static/js/main.02387569.chunk.js"
+  },
+  {
+    "revision": "7173cfe7840a5f2d3853b4220f6cf851",
+    "url": "/blue-stack/static/media/img2.7173cfe7.png"
+  },
+  {
+    "revision": "90640a8778af0217304f5effb1aa8413",
+    "url": "/blue-stack/static/media/img3.90640a87.png"
+  },
+  {
+    "revision": "977ecf7be74437c437106e00ad13f379",
+    "url": "/blue-stack/static/media/img4.977ecf7b.png"
+  },
+  {
+    "revision": "2d7f602aa6dedd9788de",
+    "url": "/blue-stack/static/css/2.aa9e3d72.chunk.css"
+  },
+  {
+    "revision": "11bb5f763d2d7fe2e3ba489605afc927",
+    "url": "/blue-stack/index.html"
+  }
+];
